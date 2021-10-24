ba415ae9a01820115da46b79a6ca427ce21af59d
TheCrazyInsanity <39921530+TheCrazyInsanity@users.noreply.github.com>
2021-10-24 13:41:24 -0400

    Switch JoinScript with Guid
    No parsing necessary, for real this time! (hopefully)

--- Program.cs
+++ Program.cs
@@ -132,7 +132,7 @@ namespace kk33.RbxStreamSniper
                                 {
                                     //Console.WriteLine();
                                     //Console.WriteLine();
-                                    Console.WriteLine(server.gameInstanceId);
+                                    Console.WriteLine(server.Guid);
                                     //Console.WriteLine();
                                     Environment.Exit(0);
                                 }
